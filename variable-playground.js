cae5e98f2f00bc0c4432d58d113211f17421af83
Eric Melzer <emelzer@gmail.com>
2016-09-27 22:48:53 -0600

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,38 @@
+// var person = {
+// 	name: 'eric',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// 	obj = {
+// 	// 		name: 'eric',
+// 	// 		age: 41
+// 	// 	};
+// 	obj.age = 41;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [88, 75];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	// gradesArr = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
+
+
+// to use debugger
+// node debug variable-playground.js
+// cont to continue
+// repl to change stuff
+// control C to leave repl
+// kill to terminate program
+// quit to exit debugger
