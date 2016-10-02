fda1653e00a4fac403ebb944c4dcc999aa4dcc05
Eric Melzer <emelzer@gmail.com>
2016-10-01 18:04:51 -0600

    update to add all files to previous

---
+++ db.js
@@ -0,0 +1,16 @@
+// db.js creates a new sqlite database
+// loads in todo model
+//exports db object with todo, sequelize instance and Sequalize library
+
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+	'dialect': 'sqlite',
+	'storage': __dirname + '/data/dev-todo-api.sqlite'
+});
+var db = {};
+
+db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+module.exports = db;
