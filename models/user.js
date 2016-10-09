3f5efca84b4dd8dab6823ec4c81143372bfa49cd
Eric Melzer <emelzer@gmail.com>
2016-10-09 15:17:48 -0600

    Add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcryptjs = require('bcryptjs');
 var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -26,7 +26,7 @@ module.exports = function(sequelize, DataTypes) {
 			set: function(value) {
 				var salt = bcryptjs.genSaltSync(10);
 				var hashedPassword = bcryptjs.hashSync(value, salt);
-				
+
 				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
@@ -40,11 +40,35 @@ module.exports = function(sequelize, DataTypes) {
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcryptjs.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			}
+		},
 		instanceMethods: {
-			toPublicJSON: function () {
+			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
+
+	return user;
 };
